f4e1d39bb634b842113339ed1e52d4ffdf5f75d7
jbarget <dear.justen@gmail.com>
2017-04-03 18:43:58 +0100

    adding Poison module for JSON parsing. #148

--- mix.exs
+++ mix.exs
@@ -42,10 +42,11 @@ defmodule Tudo.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.11.1"},
      {:excoveralls, "~> 0.6", only: :test},
      {:ueberauth, "~> 0.4"},
-     {:ueberauth_github, "~> 0.4.1"}]
+     {:ueberauth_github, "~> 0.4.1"},
+     {:httpoison, "~> 0.11.1"},
+     {:poison, "~> 2.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
