3b7f68fec7252d4086515589a8af3f9bcda36e88
Cleo <cleo.pearson@hotmail.co.uk>
2017-07-31 13:20:12 +0100

    Retrieves all repos and creates tests, #149

--- web/controllers/github_controller.ex
+++ web/controllers/github_controller.ex
@@ -3,29 +3,42 @@ defmodule Tudo.GithubController do
   use HTTPoison.Base
 
   alias Tudo.GithubController
+  alias Poison.Parser
 
   @access_token System.get_env "GITHUB_ACCESS_TOKEN"
 
   def process_url(url) do
-    "https://api.github.com" <> url <> "?access_token=" <> @access_token
+    "https://api.github.com" <> url <> "&access_token=" <> @access_token <> "&per_page=100"
   end
 
-  def index(conn, %{"org" => org}) do
-    {:ok, res} = GithubController.get("/orgs/#{org}/repos")
-
-    res.body
-    |> parse_json
-    |> get_issues
-
+  def index(conn, _) do
+    get_repos([], 1)
+    # |> get_issues
     conn
   end
 
-  defp get_issues(repos) do
-    # for each repo go and get the issues
+  def get_repos(repos, page_number) do
+    {:ok, res} = GithubController.get("/orgs/dwyl/repos?page=" <>  Integer.to_string(page_number))
+    res
+    |> parse_json
+    |> fn ({:ok, body}) -> repos ++ body end.()
+    |> check_pages(page_number)
   end
 
-  defp parse_json(body) do
-    {:ok, Poison.Parser.parse!(body)}
+  defp check_pages(repos, page_number) do
+   case rem(length(repos), 100) do
+      0 -> get_repos(repos, (page_number + 1))
+      _ -> repos
+   end
+
+  end
+
+  defp get_issues({:ok, repos}) do
+    Enum.map(repos, fn(repo) -> IO.puts repo["name"] end)
+  end
+
+  defp parse_json(res) do
+    {:ok, Parser.parse!(res.body)}
   end
 
 end
